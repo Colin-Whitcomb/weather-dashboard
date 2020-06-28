7c96221606263dbc312b73aed6b1d05601547016
Colin Whitcomb <colinsw@berkeley.edu>
2020-06-27 19:12:10 -0700

    Added current day ajax request.

--- script.js
+++ script.js
@@ -8,19 +8,39 @@ $(document).ready(function () {
         var cityName = $(this).siblings("#textArea").val();
         // check if we've collected it 
         console.log(cityName);
-        // a1a1a30d32db2a10c854b322fa1094ec = API Key 
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=a1a1a30d32db2a10c854b322fa1094ec";
-        // clear the text once they press search
-        $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response) {
-                console.log(response);
+
+       var queryURLa =  "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=a1a1a30d32db2a10c854b322fa1094ec";
+       
+       $.ajax({
+        url: queryURLa,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        
+        // store the city name in localstorage 
+        localStorage.setItem(cityName, cityName);
+        // clear text area
+        $('#textArea').val('');
+
+
+
+
+
+    });
+        // // This will be for the 5 day forcast 
+        // // a1a1a30d32db2a10c854b322fa1094ec = API Key 
+        // var queryURLb = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=a1a1a30d32db2a10c854b322fa1094ec";
+        // // clear the text once they press search
+        // $.ajax({
+        //         url: queryURLb,
+        //         method: "GET"
+        //     }).then(function(response) {
+        //         console.log(response);
                 
-                // store the city name in localstorage 
-                localStorage.setItem(cityName, cityName);
-                // clear text area
-                $('#textArea').val('');
-            });
+        //         // store the city name in localstorage 
+        //         localStorage.setItem(cityName, cityName);
+        //         // clear text area
+        //         $('#textArea').val('');
+        //     });
     });
 })
