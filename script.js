e5f33f0eb249f6fb0fd28eda70eb36eb8af099a5
Colin Whitcomb <colinsw@berkeley.edu>
2020-06-27 17:14:59 -0700

    Added js to collect and store City Name input in localStorage.

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
+    // once search button is clicked, do the following work: 
+    $("#search").on("click", function () {
+        // check if click function works
+        console.log("test 1");
+        // collect what has been written in the text area 
+        var cityName = $(this).siblings(".description").val();
+        // check if we've collected it 
+        console.log(cityName);
+        // store the city name in localstorage 
+        localStorage.setItem("Response", cityName);
+        //call aJax
+        callaJax();
+    });
 
-$("#search").on("click", function(event) {
-    console.log("test 1");
-});
+    // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    // a1a1a30d32db2a10c854b322fa1094ec = API Key 
+    var queryURL = "http:api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=a1a1a30d32db2a10c854b322fa1094ec";
+
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        }) // function taking in the response from ajax
+        .then(function (response) {
+            var results = response.data;
+            console.log(results);
+
+
+
+        });
 
 })
