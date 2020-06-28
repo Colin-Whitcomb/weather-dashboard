aec15b2e6cf33a05b4a063bf018227dfd182a58e
Colin Whitcomb <colinsw@berkeley.edu>
2020-06-27 17:42:23 -0700

    Added clear text.

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ $(document).ready(function () {
 
     // once search button is clicked, do the following work: 
     $("#search").on("click", function () {
-        // check if click function works
-        console.log("test 1");
         // collect what has been written in the text area 
-        var cityName = $(this).siblings(".description").val();
+        var cityName = $(this).siblings("#textArea").val();
+        // clear text area
+        $('#textArea').val('');
         // check if we've collected it 
         console.log(cityName);
         // store the city name in localstorage 
-        localStorage.setItem("Response", cityName);
+        localStorage.setItem(cityName, cityName);
         //call aJax
         callaJax();
+        // clear the text once they press search
+       
+     
     });
 
     // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
@@ -25,9 +28,8 @@ $(document).ready(function () {
         .then(function (response) {
             var results = response.data;
             console.log(results);
-
-
-
         });
+
+    
 
 })
